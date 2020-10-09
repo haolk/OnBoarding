1a7462478e6062de3319f190c27231881f9ecbe8
HaoLK <haolk@foxcode.org>
2020-10-09 15:43:37 +0700

    Initial Commit

---
+++ OnBoarding/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OnBoarding
+//
+//  Created by HaoLK on 10/9/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
